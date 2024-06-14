2140a1d29b3c00cbc877db7f4346a1fd12d635a5
alabsi91 <alabsi91@gmail.com>
2024-06-14 20:22:09 +0300

    Docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alabsi91.github.io',
-  base: 'animare',
+  base: import.meta.env.MODE === 'development' ? '' : 'animare',
   integrations: [
     starlight({
       title: 'animare',
@@ -42,6 +42,63 @@ export default defineConfig({
         'github': 'https://github.com/alabsi91/animare',
         'x.com': 'https://x.com/alabsi91',
       },
+      sidebar: [
+        { label: 'Installation', link: './installation' },
+        { label: 'Usage', link: './usage' },
+        {
+          label: 'API',
+          items: [
+            {
+              label: 'animare',
+              items: [
+                { label: 'Timeline', link: './api/animare/timeline' },
+                { label: 'Group', link: './api/animare/group' },
+                { label: 'Single', link: './api/animare/single' },
+                { label: 'Loop', link: './api/animare/loop' },
+              ],
+            },
+            {
+              label: 'Plugins',
+              items: [
+                { label: 'Ease', link: './api/plugins/ease' },
+                { label: 'AutoPause', link: './api/plugins/autopause' },
+                { label: 'ScrollAnimation', link: './api/plugins/scrollanimation' },
+                { label: 'Lerp', link: './api/plugins/lerp' },
+                { label: 'VectorToColor', link: './api/plugins/vectortocolor' },
+              ],
+            },
+            {
+              label: 'React',
+              items: [
+                { label: 'useAnimare', link: './api/react/useanimare' },
+                { label: 'useLoop', link: './api/react/useloop' },
+                { label: 'useAutoPause', link: './api/react/useautopause' },
+                { label: 'useScrollAnimation', link: './api/react/usescrollanimation' },
+              ],
+            },
+            {
+              label: 'Objects',
+              items: [
+                { label: 'AnimationOptions', link: './api/objects/animationoptions' },
+                { label: 'TimelineOptions', link: './api/objects/timelineoptions' },
+                { label: 'AnimationInfo', link: './api/objects/animationinfo' },
+                { label: 'TimelineInfo', link: './api/objects/timelineinfo' },
+                { label: 'TimelineReturnObject', link: './api/objects/timelineobject' },
+              ],
+            },
+            {
+              label: 'Enums',
+              items: [
+                { label: 'AnimationTiming', link: './api/enums/animationtiming' },
+                { label: 'Direction', link: './api/enums/direction' },
+                { label: 'Event', link: './api/enums/event' },
+                { label: 'ScrollElementEdge', link: './api/enums/scrollelementedge' },
+                { label: 'ScrollAxis', link: './api/enums/scrollaxis' },
+              ],
+            },
+          ],
+        },
+      ],
     }),
     react(),
   ],
