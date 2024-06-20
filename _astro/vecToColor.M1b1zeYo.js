98f72c8196540e2d80b7c09d58a8c2631efe702a
alabsi91 <alabsi91@gmail.com>
2024-06-20 17:13:52 +0300

    Docs

---
+++ _astro/vecToColor.M1b1zeYo.js
@@ -0,0 +1,1 @@
+import{i as d}from"./animare.BjVW0jvP.js";function i(t){const n=d(t),o=n?t.x:t[0],r=n?t.y:t[1],u=n?t.z:t[2],a=n?"w"in t?t.w:void 0:t[3];return typeof a=="number"?`rgb(${Math.round(o)} ${Math.round(r)} ${Math.round(u)} / ${Math.round(a*100)}%)`:`rgb(${Math.round(o)} ${Math.round(r)} ${Math.round(u)})`}export{i as v};
